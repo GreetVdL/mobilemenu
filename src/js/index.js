d195bb901986226951a2c9a5eee74023c0870df7
Greet <vanderlindengreet@gmail.com>
2021-06-17 18:07:22 +0200

    mostly done

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,9 @@
 import "../css/style.scss";
-import Square from "./classes/Square";
 
-new Square(20, 20, 200, document.querySelector(".test"));
-console.log(process.env.SECRET);
+const burger = document.querySelector("#burger");
+const menu = document.querySelector("nav aside");
+
+burger.addEventListener("click", (event) => {
+  burger.classList.toggle("active");
+  menu.classList.toggle("active");
+});

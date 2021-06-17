d423bd6385819f261e9b7edf9209bdfaf3cd85bc
Greet <vanderlindengreet@gmail.com>
2021-06-17 21:48:08 +0200

    responsive done

--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
-# mobilemenu
+# Slicing
+
+An exercise on slicing with scss and creating a mobile menu.
+
+**_Design:_**
+
+![design image](./assets/design.png)
+
+**_Deployment via surge:_**
+
+[https://mobilemenu.surge.sh/](https://mobilemenu.surge.sh/)
--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,10 @@ import "../css/style.scss";
 
 const burger = document.querySelector("#burger");
 const menu = document.querySelector("nav aside");
+const header = document.querySelector("header");
 
 burger.addEventListener("click", (event) => {
   burger.classList.toggle("active");
   menu.classList.toggle("active");
+  header.classList.toggle("active");
 });
